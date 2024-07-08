a1230d989327c9b57278dd26c83bc7ca0feedb10
dhanraj11296 <nxtwave@ip-10-2-33-39.ap-south-1.compute.internal>
2024-07-08 12:10:11 +0000

    Solution

---
+++ src/components/BlogList/index.js
@@ -0,0 +1,13 @@
+import BlogItem from '../BlogItem'
+import './index.css'
+const BlogList = props => {
+  const {blogsList} = props
+  return (
+    <ul className="blogList">
+      {blogsList.map(eachBlog => (
+        <BlogItem key={eachBlog.id} blogDetails={eachBlog} />
+      ))}
+    </ul>
+  )
+}
+export default BlogList
---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+const UserInfo = () => (
+  <div className="userInfoContainer">
+    <img
+      className="profileImg"
+      alt="profile"
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+    />
+    <h1 className="userName">Wade Warren</h1>
+    <p className="userDesignation">Software developer at UK</p>
+  </div>
+)
+export default UserInfo
